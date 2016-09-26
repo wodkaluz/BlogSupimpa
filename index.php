3578a5b33d207cba99b43a964dc6088a7566e931
wodkaluz <git@lucasluz.com>
2016-09-25 21:37:23 -0300

    Site

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta name="author" content="Lucas Luz e Fernando Raia">
+      <meta name="description" content="Um Blog de Humor">
+      <meta name="keywords" content="blog, humor">
+      <link rel="stylesheet" type="text/css" href="estilo.css">
+      <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+        <img id="imagem" src="imagens/title.png" alt="logo">
+        <nav>
+        <ul class="menu">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="contato.html">Contato</a></li>
+        </ul>
+        </nav>
+        </header>
+    </body>
+</html>
